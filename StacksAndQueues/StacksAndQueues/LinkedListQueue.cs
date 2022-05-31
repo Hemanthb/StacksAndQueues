b45a7c0773803abd5a91b950f7b5dbd6a15f5fae
hem_b <hemanthb93@gmail.com>
2022-05-31 17:16:19 +0530

    [Hemanth]:Enqueue a queue

---
+++ StacksAndQueues/StacksAndQueues/LinkedListQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class LinkedListQueue
+    {
+        Node front = null;
+        public void Enqueue(int data)
+        {
+            
+            Node node = new Node(data);
+            if(front == null)
+            {
+                front = node;
+                Console.WriteLine("Enqueued Element is {0}", node.data);
+            }
+            else
+            {
+                Node rear = front;
+                while(rear.next != null)
+                {
+                    rear = rear.next;
+                }
+                rear.next = node;
+                Console.WriteLine("Enqueued Element is {0}", node.data);
+            }
+            
+        }
+        public void DisplayQueue()
+        {
+            if(this.front == null)
+            {
+                Console.WriteLine("Queue is Empty!!");
+            }
+            else
+            {
+                Console.WriteLine();
+                Node temp = this.front;
+                while(temp != null)
+                {
+                    Console.Write(temp.data + " -> ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -1,9 +1,17 @@
 ﻿StacksAndQueues.LinkedListStack stack = new StacksAndQueues.LinkedListStack();
-stack.push(70);
+StacksAndQueues.LinkedListQueue queue = new StacksAndQueues.LinkedListQueue();
+/*stack.push(70);
 stack.push(30);
 stack.push(56);
 stack.Display();
 stack.peek();
 stack.pop();
 stack.Display();
-stack.IsEmpty();
+stack.IsEmpty();*/
+
+//Enqueue and Dequeue
+
+queue.Enqueue(56);
+queue.Enqueue(30);
+queue.Enqueue(70);
+queue.DisplayQueue();
