58e3dd050e855056f21aed3341b40b7f2fb1e7bf
hem_b <hemanthb93@gmail.com>
2022-05-31 16:43:52 +0530

    [Hemanth]:Stack peep,pop and finally emptied

--- StacksAndQueues/StacksAndQueues/LinkedListStack.cs
+++ StacksAndQueues/StacksAndQueues/LinkedListStack.cs
@@ -38,5 +38,36 @@ namespace StacksAndQueues
                 temp = temp.next;
             }
         }
+        public void peek()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack Is Empty!!");
+            }
+            else
+            {
+                Console.WriteLine("Top element of stack is"+this.top.data);
+            }
+        }
+        public void pop()
+        {
+            peek();
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is Empty and Nothing to Pop!!");
+                return;
+            }
+            Console.WriteLine("Element to be Popped is - " + this.top.data);
+            this.top = this.top.next;   
+        }
+
+        public void IsEmpty()
+        {
+            while(this.top != null)
+            {
+                pop();
+            }
+            Console.WriteLine("\n Stack Is Emptied \n");
+        }
     }
 }
--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -3,3 +3,7 @@ stack.push(70);
 stack.push(30);
 stack.push(56);
 stack.Display();
+stack.peek();
+stack.pop();
+stack.Display();
+stack.IsEmpty();
